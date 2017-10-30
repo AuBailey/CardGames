ebf1c20d7e19ba2b1c5fa6b42bc0f2719a9af9c2
Parker Dunbar <PDunbar@student.neumont.edu>
2017-10-30 09:44:20 -0600

    Switching GUIs

---
+++ card-games/src/main/java/games/csc180/controller/Run.java
@@ -0,0 +1,13 @@
+package games.csc180.controller;
+
+import games.csc180.view.MainScreen;
+import javafx.application.Application;
+
+public class Run {
+
+	public static void main(String[] args) {
+		Application.launch(MainScreen.class);
+
+	}
+
+}
