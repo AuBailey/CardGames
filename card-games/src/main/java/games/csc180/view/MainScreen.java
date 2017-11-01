7a7af1c15efcc73cb2c8983e70a94b41b60c054d
Parker Dunbar <PDunbar@student.neumont.edu>
2017-11-01 08:53:28 -0600

    More War GUI Stuff

--- card-games/src/main/java/games/csc180/view/MainScreen.java
+++ card-games/src/main/java/games/csc180/view/MainScreen.java
@@ -3,27 +3,30 @@ package games.csc180.view;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class MainScreen extends Application {
-	private Stage primaryStage;
+	private Stage primaryStage, popupStage;
 	private Scene mainScene;
-	
+	private Parent popup;
+
 	@FXML
-	private Button startWar, quitWar;
+	private Button initWar, startWar, quitWar;
 
 	@Override
 	public void start(Stage stage) throws Exception {
 		primaryStage = stage;
 		mainScreen(primaryStage);
 	}
-	
+
 	private void mainScreen(Stage stage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("SelectGame.fxml"));
 		mainScene = new Scene(root);
@@ -33,16 +36,26 @@ public class MainScreen extends Application {
 		primaryStage.setMaximized(true);
 		primaryStage.show();
 	}
-	
+
+	@FXML
+	private void initWar(ActionEvent e) throws IOException {
+		popupStage = new Stage();
+		popup = FXMLLoader.load(getClass().getResource("WarPopup.fxml"));
+		popupStage.setScene(new Scene(popup));
+		popupStage.setTitle("War Options");
+		popupStage.initModality(Modality.NONE);
+		popupStage.initOwner(initWar.getScene().getWindow());
+		popupStage.showAndWait();
+
+	}
+
 	@FXML
 	private void startWar(ActionEvent e) throws IOException {
+		
 		Scene pScene = startWar.getScene();
 		Parent war = FXMLLoader.load(getClass().getResource("War.fxml"));
 		mainScene = pScene;
 		mainScene.setRoot(war);
 	}
-	
-	
-	
 
 }
